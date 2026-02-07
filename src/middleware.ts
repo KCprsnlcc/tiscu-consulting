3f62198a2f31152fb5958a314833a18e7ff493f4
Khadaffe Abubakar Sulaiman <95871722+KCprsnlcc@users.noreply.github.com>
2026-02-08 02:33:07 +0800

    Fix robots.txt accessibility by adding txt extension to middleware exclusion pattern

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,7 +23,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|xml)).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|xml|txt)).*)",
     "/(api|trpc)(.*)",
   ],
 };
